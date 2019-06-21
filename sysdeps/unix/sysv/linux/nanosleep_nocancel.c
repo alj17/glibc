9e3d95ad1f70909a787dd11fbc735bcafa02050e
Alistair Francis <alistair.francis@wdc.com>
2019-06-21 13:00:23 -0700

    sysdeps/nanosleep: Use clock_nanosleep_time64 if avaliable
    The nanosleep syscall is not supported on newer 32-bit platforms (such
    as RV32). To fix this issue let's use clock_nanosleep_time64 if it is
    avaliable.
    
    Let's use CLOCK_REALTIME when calling clock_nanosleep_time64 as the
    Linux specification says:
      "POSIX.1 specifies that nanosleep() should measure time against the
       CLOCK_REALTIME clock. However, Linux measures the time using the
       CLOCK_MONOTONIC clock. This probably does not matter, since the POSIX.1
       specification for clock_settime(2) says that discontinuous changes in
       CLOCK_REALTIME should not affect nanosleep()"
    
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>
    
    	* nptl/thrd_sleep.c: Use clock_nanosleep_time64 instead of nanosleep.
    	* sysdeps/unix/sysv/linux/nanosleep.c: Likewise.
    	* sysdeps/unix/sysv/linux/clock_nanosleep.c: Likewise.
    	* sysdeps/unix/sysv/linux/nanosleep_nocancel.c: Likewise.

--- sysdeps/unix/sysv/linux/nanosleep_nocancel.c
+++ sysdeps/unix/sysv/linux/nanosleep_nocancel.c
@@ -21,9 +21,69 @@
 #include <not-cancel.h>
 
 int
-__nanosleep_nocancel (const struct timespec *requested_time,
-		      struct timespec *remaining)
+__nanosleep_nocancel_time64 (const struct __timespec64 *requested_time,
+                             struct __timespec64 *remaining)
 {
-  return INLINE_SYSCALL_CALL (nanosleep, requested_time, remaining);
+#ifdef __ASSUME_TIME64_SYSCALLS
+# ifndef __NR_clock_nanosleep_time64
+#  define __NR_clock_nanosleep_time64 __NR_clock_nanosleep
+# endif
+  return INLINE_SYSCALL_CALL (clock_nanosleep_time64, CLOCK_REALTIME, 0,
+                              requested_time, remaining);
+#else
+# ifdef __NR_clock_nanosleep_time64
+  long int ret_64;
+
+  ret_64 = INLINE_SYSCALL_CALL (clock_nanosleep_time64, CLOCK_REALTIME, 0,
+                                requested_time, remaining);
+
+  if (ret_64 == 0 || errno != ENOSYS)
+    return ret_64;
+# endif /* __NR_clock_nanosleep_time64 */
+  int ret;
+  struct timespec ts32, tr32;
+
+  if (! in_time_t_range (requested_time->tv_sec))
+    {
+      __set_errno (EOVERFLOW);
+      return -1;
+    }
+
+  ts32 = valid_timespec64_to_timespec (requested_time);
+  ret = INLINE_SYSCALL_CALL (nanosleep, &ts32, &tr32);
+
+  if (ret == 0 || errno != ENOSYS)
+    *remaining = valid_timespec_to_timespec64 (&tr32);
+
+  return ret;
+#endif /* __ASSUME_TIME64_SYSCALLS */
 }
+
+#if __TIMESIZE != 64
+int
+__nanosleep_nocancel (const struct timespec *requested_time,
+                      struct timespec *remaining)
+{
+  int ret;
+  timespec64 treq64, trem64;
+
+  treq64 = valid_timespec_to_timespec64 (req);
+  ret = __nanosleep_nocancel_time64 (&treq64, &trem64);
+
+  if (ret == 0 || errno != ENOSYS)
+    {
+      if (! in_time_t_range (trem64->tv_sec))
+        {
+          __set_errno (EOVERFLOW);
+          return -1;
+        }
+
+      if (remaining)
+        *remaining = valid_timespec64_to_timespec (&tr32);
+    }
+
+  return ret;
+}
+#endif
+
 hidden_def (__nanosleep_nocancel)
